a519dda36d7e3677efd8a93ca4445fea33836d3a
Ray Deck <ray@raydeck.com>
2020-06-18 07:16:26 -0500

    Update version when major or minor changes, or if build is in advance. Otherwise, leave alone to allow for build-level updates

--- packages/fastlane/setversion.js
+++ packages/fastlane/setversion.js
@@ -5,6 +5,14 @@ const { version } = getAPI();
 const packagePath = join(__dirname, "package.json");
 const o = JSON.parse(readFileSync(packagePath, { encoding: "utf8" }));
 if (o.version !== version) {
-  o.version = version;
-  writeFileSync(packagePath, JSON.stringify(o, null, 2));
+  const [major, minor, build] = version.split(".");
+  const [packagemajor, packageminor, packagebuild] = o.version(split);
+  if (
+    major > packagemajor ||
+    (major === packagemajor && minor > packageminor) ||
+    (major === packagemajor && minor === packageminor && build > packagebuild)
+  ) {
+    o.version = version;
+    writeFileSync(packagePath, JSON.stringify(o, null, 2));
+  }
 }
