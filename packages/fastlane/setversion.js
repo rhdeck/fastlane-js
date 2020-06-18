93494d5996bc715eaebfb48b376163edfc50bd78
Ray Deck <ray@raydeck.com>
2020-06-18 07:55:51 -0500

    Fixing version setter

--- packages/fastlane/setversion.js
+++ packages/fastlane/setversion.js
@@ -6,7 +6,7 @@ const packagePath = join(__dirname, "package.json");
 const o = JSON.parse(readFileSync(packagePath, { encoding: "utf8" }));
 if (o.version !== version) {
   const [major, minor, build] = version.split(".");
-  const [packagemajor, packageminor, packagebuild] = o.version(split);
+  const [packagemajor, packageminor, packagebuild] = o.version.split(".");
   if (
     major > packagemajor ||
     (major === packagemajor && minor > packageminor) ||
