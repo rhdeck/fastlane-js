f134f1305322478a890acd0dc240c2a68c606c1b
Ray Deck <ray@raydeck.com>
2020-06-18 07:31:13 -0500

    Removing unnecessary dependencies (e.g. es6-deferred)

--- packages/fastlane-base/src/es6-deferred.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "es6-deferred" {
-  class Deferred {
-    resolve: (...args: any) => void;
-    reject: (reason: any) => void;
-    promise: Promise<any>;
-  }
-  export = Deferred;
-}
